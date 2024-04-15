c22fe03bd5f8d2a558efad32043b8f326c4d8e9a
nqb3010 <nqb3010@gmail.com>
2024-04-15 11:18:28 +0700

    version 1.0.0

---
+++ main.py
@@ -0,0 +1,88 @@
+import pygame
+import math
+import sys
+# Khởi tạo Pygame
+pygame.init()
+angle = 0  # Góc ban đầu
+radius = 90  # Bán kính của hình tròn
+center_x = 120  # Tọa độ x của tâm hình tròn
+center_y = 300  # Tọa độ y của tâm hình tròn
+# Tạo màn hình
+speed = 1
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# Tạo hình ảnh
+image = pygame.image.load("piston1.png")
+
+# Thu nhỏ ảnh piston về kích thước 150x150
+image = pygame.transform.scale(image, (150, 150))
+lines = [((400, 240), (400, 260)),
+          ((400, 260), (800, 260)),  # Đoạn thẳng từ (100, 100) đến (200, 200)
+         ((800, 260), (800, 345)),  # Đoạn thẳng từ (300, 300) đến (500, 100)
+         ((800, 345), (400, 345)),
+         ((400, 365), (400, 345))]   # Đoạn thẳng từ (50, ((800, 345), (500, 345))200) đến (400, 100)
+
+# Vị trí ban đầu của hình ảnh
+image_width = image.get_width()
+image_height = image.get_height()
+x = 400
+y = (screen_height - image_height) / 2
+increasing = True
+# Biến kiểm soát vòng lặp chính
+running = True
+# Vòng lặp chính
+while True:
+
+
+
+    # Xử lý sự kiện
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                running = False
+            elif event.key == pygame.K_UP:
+                speed += 1  # Tăng tốc độ quay khi nhấn phím mũi tên lên
+            elif event.key == pygame.K_DOWN:
+                     if speed >0:
+                            speed -= 1  # Giảm tốc độ quay khi nhấn phím mũi tên xuống
+                # Xóa màn hình
+    screen.fill((0, 0, 0))
+    #
+    # Vẽ các đoạn thẳng
+    for line in lines:
+        pygame.draw.line(screen, (255, 255, 255), line[0], line[1], 6)
+
+    point_x = center_x + int(radius * math.cos(math.radians(angle)))
+    point_y = center_y + int(radius * math.sin(math.radians(angle)))
+
+    # Vẽ hình ảnh lên màn hình
+    if x >= 650:
+        increasing = False  # Khi x đạt 650, thay đổi hướng giảm dần
+    if increasing:
+        pygame.time.delay(10)
+        x += 5.3 * speed  # Tăng giá trị của x nếu đang trong quá trình tăng
+    else:
+        pygame.time.delay(10)
+        x -= 5.3 * speed  # Giảm giá trị của x nếu đang trong quá trình giảm
+
+    if x <= 400:  # Khi x giảm về 299, thoát khỏi vòng lặp
+        increasing = True
+    pygame.time.delay(10)
+    angle += 2 * speed
+    if angle >= 360:
+        angle = 0
+    # Vẽ dây nối từ trục quay tới hình ảnh piston
+    pygame.draw.line(screen, (255, 255, 255), (point_x, point_y), (x + image_width / 2, y + image_height / 2), 5)
+    print(point_x, point_y)
+    # Vẽ hình tròn và điểm trên hình tròn
+    pygame.draw.circle(screen, (255, 255, 255), (center_x, center_y), radius, 1)
+    pygame.draw.circle(screen, (255, 0, 0), (int(point_x), int(point_y)), 5)
+
+    screen.blit(image, (x, y))
+    pygame.display.update()
